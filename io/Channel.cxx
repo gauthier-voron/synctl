44aba22c4ecbe0a3bf5b00053a8676ad99faf6f0
Gauthier Voron <gauthier.voron@sydney.edu.au>
2019-03-07 22:43:55 +1100

    feat(io): add the 'ssh://' protocol recognition to Channel::open()
    Add the ability for Channel::open() to detect the 'ssh://' prefix on a
    server specification and to open an SshChannel in this case.

--- io/Channel.cxx
+++ io/Channel.cxx
@@ -5,6 +5,7 @@
 
 #include "synctl/io/ChannelException.hxx"
 #include "synctl/io/FileChannel.hxx"
+#include "synctl/io/SshChannel.hxx"
 
 
 using std::string;
@@ -12,12 +13,18 @@ using std::unique_ptr;
 using synctl::Channel;
 using synctl::ChannelException;
 using synctl::FileChannel;
+using synctl::SshChannel;
 
 
 unique_ptr<Channel> Channel::open(const string &address)
 {
+	SshChannel::OpenSettings sshSettings;
+
 	if (address.find("file://") == 0) {
 		return FileChannel::open(address.substr(7));
+	} else if (address.find("ssh://") == 0) {
+		sshSettings.location = address.substr(6);
+		return SshChannel::open(sshSettings);
 	}
 
 	throw ChannelException(address);
