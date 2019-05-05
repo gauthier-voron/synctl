9c46dece1a73397f540be89026e76b13db83cec7
Gauthier Voron <gauthier.voron@mnesic.fr>
2019-05-05 22:44:40 +1000

    refactor(plan): remove duplicated code for snapshot merging
    Remove the code for merging snapshots from Receive_1 and use the one in
    SnapshotCombiner instead.

--- include/synctl/plan/Receive_1.hxx
+++ include/synctl/plan/Receive_1.hxx
@@ -17,69 +17,23 @@ namespace synctl {
 
 class Directory_1;
 class InputStream;
+class Link_1;
 class Repository;
+class SnapshotCombiner;
 
 
 class Receive_1
 {
-	struct Baseref
-	{
-		Reference  reference;
-		opcode_t   opcode;
-		bool       merged = false;
-		Reference  mergeAs;
-
-		Baseref() = default;
-		Baseref(const Reference &reference, opcode_t opcode);
-		Baseref(const Baseref &other) = default;
-		Baseref(Baseref &&other) = default;
-	};
-
 	struct Context
 	{
-		std::map<std::string, std::unique_ptr<Baseref>>    *baserefs;
-		std::map<Reference, std::unique_ptr<Directory_1>>  *basedirs;
-		std::map<Reference, std::unique_ptr<Directory_1>>  *recvdirs;
+		SnapshotCombiner                                   *scombiner;
 		InputStream                                        *input;
 		Repository                                         *repository;
-	};
-
-	struct MergeContext
-	{
-		std::string     path;      // path of the merged directory
-		Baseref        *baseref;   // ref/opcode in the base snapshot
-		Directory_1    *basedir;   // directory in the base snapshot
-		Directory_1    *recvdir;   // directory received from client
-		const Context  *rcontext;
 	};
 
 
 	Snapshot::Content   _base;
 	Filter             *_filter = nullptr;
-
-
-	Filter::Action _filterPath(const std::string &path) const;
-
-	Baseref *_loadBasedir(MergeContext *context, const std::string &name)
-		const;
-
-	void _findBasedir(MergeContext *context) const;
-
-	void _loadRecvdir(MergeContext *context, const Reference &ref) const;
-
-	void _findRecvdir(MergeContext *context, const Reference &ref) const;
-
-	bool _loadBaseref(MergeContext *context) const;
-
-	bool _findBaseref(MergeContext *context) const;
-
-	void _storeDirectory(const Context *context, const Directory_1 &dir,
-			     const Reference &reference);
-
-	void _storeDirectory(const Context *context, const Directory_1 &dir,
-			     Reference *reference = nullptr);
-
-	bool _mergeDirectory(MergeContext *context);
 
 
 	bool _receiveEntry(const Context *context);
--- plan/Receive_1.cxx
+++ plan/Receive_1.cxx
@@ -1,15 +1,14 @@
 #include "synctl/plan/Receive_1.hxx"
 
-#include <map>
 #include <memory>
 #include <string>
-#include <vector>
 
 #include "synctl/io/HashOutputStream.hxx"
 #include "synctl/io/LimitedInputStream.hxx"
 #include "synctl/io/Path.hxx"
 #include "synctl/io/TransientOutputStream.hxx"
 #include "synctl/plan/Opcode.hxx"
+#include "synctl/plan/SnapshotCombiner.hxx"
 #include "synctl/repo/OverwriteException.hxx"
 #include "synctl/repo/Repository.hxx"
 #include "synctl/tree/Directory_1.hxx"
@@ -20,281 +19,20 @@
 #include "synctl/tree/Symlink_1.hxx"
 
 
-using std::make_unique;
-using std::map;
-using std::move;
 using std::string;
 using std::unique_ptr;
-using std::vector;
 using synctl::Directory_1;
 using synctl::Filter;
 using synctl::HashOutputStream;
 using synctl::LimitedInputStream;
-using synctl::Link_1;
-using synctl::Linktable_1;
 using synctl::OverwriteException;
 using synctl::Receive_1;
 using synctl::Regular_1;
 using synctl::Repository;
+using synctl::SnapshotCombiner;
 using synctl::Symlink_1;
 using synctl::TransientOutputStream;
 
-
-Receive_1::Baseref::Baseref(const Reference &_reference, opcode_t _opcode)
-	: reference(_reference), opcode(_opcode)
-{
-}
-
-Receive_1::Baseref *Receive_1::_loadBasedir(MergeContext *context,
-					    const string &name) const
-{
-	unique_ptr<Directory_1> dir = make_unique<Directory_1>();
-	const Context *rc = context->rcontext;
-	unique_ptr<InputStream> input;
-	unique_ptr<Baseref> uptr;
-	Baseref *base;
-	string cpath;
-	size_t cplen;
-
-	input = rc->repository->readObject(context->baseref->reference);
-	if (input == nullptr)
-		return nullptr;
-
-	context->basedir = dir.get();
-	dir->read(input.get());
-
-	if (context->path[context->path.length() - 1] == '/')
-		cpath = context->path;
-	else
-		cpath = context->path + "/";
-	cplen = cpath.length();
-
-	for (const Directory_1::Entry &child : dir->getChildren()) {
-		cpath += child.name;
-
-		uptr = make_unique<Baseref>(child.reference, child.opcode);
-		if (name == child.name)
-			base = uptr.get();
-		rc->baserefs->emplace(make_pair(cpath, move(uptr)));
-
-		cpath.resize(cplen);
-	}
-
-	rc->basedirs->emplace
-		(make_pair(context->baseref->reference, move(dir)));
-	return base;
-}
-
-void Receive_1::_findBasedir(MergeContext *context) const
-{
-	const Context *rc = context->rcontext;
-	auto it = rc->basedirs->find(context->baseref->reference);
-
-	if (it != rc->basedirs->end())
-		context->basedir = it->second.get();
-
-	_loadBasedir(context, "");
-}
-
-void Receive_1::_loadRecvdir(MergeContext *context, const Reference &ref) const
-{
-	unique_ptr<Directory_1> dir = make_unique<Directory_1>();
-	const Context *rc = context->rcontext;
-	unique_ptr<InputStream> input;
-
-	input = rc->repository->readObject(ref);
-
-	context->recvdir = dir.get();
-	dir->read(input.get());
-
-	rc->basedirs->emplace(make_pair(ref, move(dir)));
-}
-
-void Receive_1::_findRecvdir(MergeContext *context, const Reference &ref) const
-{
-	const Context *rc = context->rcontext;
-	auto it = rc->basedirs->find(ref);
-
-	if (it != rc->basedirs->end()) {
-		context->recvdir = it->second.get();
-	} else {
-		_loadRecvdir(context, ref);
-	}
-}
-
-bool Receive_1::_loadBaseref(MergeContext *context) const
-{
-	unique_ptr<Baseref> uptr;
-	MergeContext mctx;
-	const Context *rc;
-	string name;
-
-	if (context->path == "/") {
-		uptr = make_unique<Baseref>(_base.tree, _base.opcode);
-		context->baseref = uptr.get();
-
-		rc = context->rcontext;
-		rc->baserefs->emplace(make_pair(context->path, move(uptr)));
-
-		return true;
-	}
-
-	mctx.rcontext = context->rcontext;
-	split(context->path, &mctx.path, &name);
-
-	if (_findBaseref(&mctx) == false)
-		return false;
-	if (mctx.baseref->opcode != OP_TREE_DIRECTORY_1)
-		return false;
-
-	context->baseref = _loadBasedir(&mctx, name);
-
-	return (context->baseref != nullptr);
-}
-
-bool Receive_1::_findBaseref(MergeContext *context) const
-{
-	auto it = context->rcontext->baserefs->find(context->path);
-
-	if (it != context->rcontext->baserefs->end()) {
-		context->baseref = it->second.get();
-		return true;
-	}
-
-	return _loadBaseref(context);
-}
-
-void Receive_1::_storeDirectory(const Context *context, const Directory_1 &dir,
-				const Reference &reference)
-{
-	unique_ptr<TransientOutputStream> tos;
-
-	for (Directory_1::Entry &child : dir.getChildren())
-		context->repository->takeReference(child.reference);
-
-	tos = context->repository->newObject();
-	dir.write(tos.get());
-
-	try {
-		tos->commit(reference);
-	} catch (OverwriteException &e) {
-		// May happen legally after a merge
-	}
-}
-
-void Receive_1::_storeDirectory(const Context *context, const Directory_1 &dir,
-				Reference *reference)
-{
-	unique_ptr<TransientOutputStream> tos;
-	Reference ref;
-
-	for (Directory_1::Entry &child : dir.getChildren())
-		context->repository->takeReference(child.reference);
-
-	tos = context->repository->newObject();
-
-	if (reference == nullptr)
-		reference = &ref;
-	dir.write(tos.get(), reference);
-
-	try {
-		tos->commit(*reference);
-	} catch (OverwriteException &e) {
-		// May happen legally after a merge
-	}
-}
-
-bool Receive_1::_mergeDirectory(MergeContext *context)
-{
-	vector<Directory_1::Entry> baseChildren, recvChildren;
-	bool modified = false;
-	size_t rlen, i, j;
-	MergeContext mctx;
-	bool hasRecv;
-
-	_findBasedir(context);
-
-	baseChildren = context->basedir->getChildren();
-	recvChildren = context->recvdir->getChildren();
-
-	if (context->path[context->path.length() - 1] == '/')
-		mctx.path = context->path;
-	else
-		mctx.path = context->path + "/";
-	rlen = mctx.path.length();
-
-	mctx.rcontext = context->rcontext;
-
-	i = j = 0;
-	while (i < baseChildren.size()) {
-		hasRecv = j < recvChildren.size();
-		if (hasRecv && (recvChildren[j].name < baseChildren[i].name)) {
-			j++;
-			continue;
-		}
-
-		mctx.path += baseChildren[i].name;
-
-		if (!hasRecv || (recvChildren[j].name > baseChildren[i].name)){
-			if (_filterPath(mctx.path) == Filter::Reject) {
-				context->recvdir->addChild(baseChildren[i]);
-				modified = true;
-			}
-
-			i++;
-			mctx.path.resize(rlen);
-			continue;
-		}
-
-		if (recvChildren[j].opcode != OP_TREE_DIRECTORY_1)
-			goto skip;
-		if (baseChildren[i].opcode != OP_TREE_DIRECTORY_1)
-			goto skip;
-		if (baseChildren[i].reference == recvChildren[j].reference)
-			goto skip;
-
-		_findBaseref(&mctx);
-
-		if (mctx.baseref->merged == false) {
-			_findRecvdir(&mctx, recvChildren[j].reference);
-			if (_mergeDirectory(&mctx))
-				_storeDirectory(context->rcontext,
-						*mctx.recvdir,
-						&mctx.baseref->mergeAs);
-			else
-				mctx.baseref->mergeAs =
-					recvChildren[j].reference;
-		}
-
-		if (mctx.baseref->mergeAs != recvChildren[j].reference) {
-			context->recvdir->removeChild(recvChildren[j].name);
-			recvChildren[j].reference = mctx.baseref->mergeAs;
-			context->recvdir->addChild(recvChildren[j]);
-			modified = true;
-		}
-
-	 skip:
-		i++;
-		j++;
-		mctx.path.resize(rlen);
-	}
-
-	context->baseref->merged = true;
-
-	return modified;
-}
-
-
-Filter::Action Receive_1::_filterPath(const string &path) const
-{
-	Filter::Action ret;
-
-	ret = _filter->apply(path);
-	if (ret == Filter::Ignore)
-		ret = Filter::Accept;
-
-	return ret;
-}
 
 bool Receive_1::_receiveEntry(const Context *context)
 {
@@ -323,39 +61,38 @@ bool Receive_1::_receiveEntry(const Context *context)
 
 void Receive_1::_receiveDirectory(const Context *context)
 {
+	unique_ptr<TransientOutputStream> tos;
+	Reference reference, *dest;
 	LimitedInputStream lis;
-	MergeContext mctx;
 	Directory_1 dir;
 	uint64_t dlen;
-	Reference ref;
+	string path;
 
-	mctx.path = context->input->readStr();
+	path = context->input->readStr();
 	dlen = context->input->readInt<uint64_t>();
 	lis = LimitedInputStream(context->input, dlen);
-	dir.read(&lis, &ref);
+	dir.read(&lis, &reference);
 
-	if (_filter == nullptr) {
-		_storeDirectory(context, dir, ref);
-		return;
+	dest = nullptr;
+	if (context->scombiner != nullptr) {
+		if (context->scombiner->merge(path, reference, &dir))
+			dest = &reference;
 	}
 
-	mctx.rcontext = context;
+	for (Directory_1::Entry &child : dir.getChildren())
+		context->repository->takeReference(child.reference);
 
-	if ((_findBaseref(&mctx) == false) ||
-	    (mctx.baseref->opcode != OP_TREE_DIRECTORY_1) ||
-	    (mctx.baseref->reference == ref)) {
-		_storeDirectory(context, dir, ref);
-		return;
+	tos = context->repository->newObject();
+	dir.write(tos.get(), dest);
+
+	try {
+		tos->commit(reference);
+	} catch (OverwriteException &e) {
+		// May happen legally after a merge
 	}
 
-	mctx.recvdir = &dir;
-
-	if (_mergeDirectory(&mctx)) {
-		_storeDirectory(context, dir, &mctx.baseref->mergeAs);
-	} else {
-		_storeDirectory(context, dir, ref);
-		mctx.baseref->mergeAs = ref;
-	}
+	if (context->scombiner != nullptr)
+		context->scombiner->assign(path, reference);
 }
 
 void Receive_1::_receiveRegular(const Context *context)
@@ -476,17 +213,18 @@ void Receive_1::setBaseFilter(const Snapshot::Content &base, Filter *filter)
 void Receive_1::receive(InputStream *input, Repository *repository,
 			Snapshot::Content *content)
 {
-	map<string, unique_ptr<Baseref>> baserefs;
-	map<Reference, unique_ptr<Directory_1>> basedirs;
-	map<Reference, unique_ptr<Directory_1>> recvdirs;
-	MergeContext mctx;
+	SnapshotCombiner scombiner;
 	Context ctx;
 
-	ctx.baserefs = &baserefs;
-	ctx.basedirs = &basedirs;
-	ctx.recvdirs = &recvdirs;
 	ctx.input = input;
 	ctx.repository = repository;
+
+	if (_filter != nullptr) {
+		scombiner = SnapshotCombiner(repository, _filter, _base);
+		ctx.scombiner = &scombiner;
+	} else {
+		ctx.scombiner = nullptr;
+	}
 
 	while (_receiveEntry(&ctx))
 		;
@@ -494,15 +232,8 @@ void Receive_1::receive(InputStream *input, Repository *repository,
 	content->opcode = input->readInt<opcode_t>();
 	input->readall(content->tree.data(), content->tree.size());
 
-	if ((_filter != nullptr) && (content->opcode == OP_TREE_DIRECTORY_1)) {
-		mctx.path = "/";
-		mctx.rcontext = &ctx;
-
-		_findBaseref(&mctx);
-
-		if (mctx.baseref->merged)
-			content->tree = mctx.baseref->mergeAs;
-	}
+	if (ctx.scombiner != nullptr)
+		ctx.scombiner->merged("/", &content->tree);
 
 	while (_receiveLinks(&ctx))
 		;
