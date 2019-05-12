82a66f9686b5ecaf84430a1547469efc9318aa38
Gauthier Voron <gauthier.voron@mnesic.fr>
2019-05-12 14:52:48 +1000

    feat(ui): add a configuration for the server command
    Add a ConfigurationServer to copy the behavior of ActionServer but in a
    Configuration compliant form.

---
+++ ui/ConfigurationServer.cxx
@@ -0,0 +1,83 @@
+#include "synctl/ui/ConfigurationServer.hxx"
+
+#include <string>
+#include <vector>
+
+#include "synctl/io/Directory.hxx"
+#include "synctl/io/FdInputStream.hxx"
+#include "synctl/io/FdOutputStream.hxx"
+#include "synctl/io/PipeChannel.hxx"
+#include "synctl/plan/Protocol.hxx"
+#include "synctl/repo/Repository.hxx"
+#include "synctl/ui/ArgumentParser.hxx"
+#include "synctl/ui/ConfigurationBase.hxx"
+#include "synctl/ui/OperandInvalidException.hxx"
+#include "synctl/ui/OperandMissingException.hxx"
+#include "synctl/ui/OperandUnexpectedException.hxx"
+#include "synctl/ui/Path.hxx"
+
+
+using std::string;
+using std::unique_ptr;
+using std::vector;
+using synctl::ArgumentParser;
+using synctl::FdInputStream;
+using synctl::FdOutputStream;
+using synctl::ConfigurationBase;
+using synctl::ConfigurationServer;
+using synctl::OperandMissingException;
+using synctl::OperandInvalidException;
+using synctl::OperandUnexpectedException;
+using synctl::Path;
+using synctl::PipeChannel;
+using synctl::Protocol;
+using synctl::Repository;
+
+
+ConfigurationServer::ConfigurationServer(ConfigurationBase *inner)
+	: ConfigurationCommand(inner)
+{
+}
+
+string ConfigurationServer::serverPath() const
+{
+	return Path::resolvePath(_operandServerPath);
+}
+
+size_t ConfigurationServer::getOperands(const vector<string> &args)
+{
+	if (args.size() < 1)
+		throw OperandMissingException("server-path");
+
+	_operandServerPath = args[0];
+
+	return 1;
+}
+
+static int __main(ConfigurationServer *config)
+{
+	PipeChannel channel = PipeChannel(FdInputStream(0), FdOutputStream(1));
+	unique_ptr<Protocol> protocol = Protocol::serverHandcheck(&channel);
+	Repository repository = Repository(config->serverPath());
+
+	if (protocol == nullptr)
+		return 1;
+
+	protocol->serve(&repository);
+
+	channel.close();
+
+	return 0;
+}
+
+int ConfigurationServer::main(ConfigurationBase *c, const vector<string> &args)
+{
+	ConfigurationServer config = ConfigurationServer(c);
+	ArgumentParser parser = ArgumentParser(&config);
+	vector<string> rem = parser.parse(args);
+
+	if (rem.empty() == false)
+		throw OperandUnexpectedException(rem[0]);
+
+	return __main(&config);
+}
