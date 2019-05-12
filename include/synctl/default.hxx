dc5a0a57084b7d238b783258a81d2f935d77221f
Gauthier Voron <gauthier.voron@mnesic.fr>
2019-05-12 14:37:55 +1000

    feat(synctl): add a header file with program defaults
    Add a header file to define all the program specific constants and
    default values.

---
+++ include/synctl/default.hxx
@@ -0,0 +1,23 @@
+#ifndef _INCLUDE_SYNCTL_DEFAULT_HXX_
+#define _INCLUDE_SYNCTL_DEFAULT_HXX_
+
+
+#define SYNCTL_CONSTANT_NAME            "synctl"
+
+#define SYNCTL_CONSTANT_MAJOR_VERSION   0
+
+#define SYNCTL_CONSTANT_MINOR_VERSION   0
+
+#define SYNCTL_CONSTANT_VARIANT         0
+
+#define SYNCTL_CONSTANT_AUTHOR          "Gauthier Voron"
+
+#define SYNCTL_CONSTANT_MAILTO          "gauthier.voron@mnesic.fr"
+
+
+#define SYNCTL_DEFAULT_CONFIG           "/etc/synctl"
+
+#define SYNCTL_DEFAULT_TRUNK            "master"
+
+
+#endif
