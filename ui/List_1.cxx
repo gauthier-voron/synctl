60b7dc9602ef417f4a2f0eb3850ac59e0143ed87
Gauthier Voron <gauthier.voron@mnesic.fr>
2019-05-24 23:00:12 +1000

    feat(plan): add the List_1 protocol
    Add the List_1 protocol which is the client side operation for the list
    command.

---
+++ ui/List_1.cxx
@@ -0,0 +1,111 @@
+#include "synctl/plan/List_1.hxx"
+
+#include <cstdint>
+#include <ctime>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "synctl/io/InputStream.hxx"
+#include "synctl/plan/Opcode.hxx"
+
+
+using std::make_pair;
+using std::map;
+using std::move;
+using std::string;
+using std::vector;
+using synctl::InputStream;
+using synctl::List_1;
+
+
+void List_1::_listBranch(InputStream *input, Handler handler)
+{
+	map<string, vector<string>> snapshotBranches;
+	map<string, uint64_t> snapshotDates;
+	map<string, string> snapshotTrunks;
+	string branch, trunk, snapshot;
+	struct timespec ts;
+	uint64_t unixtime;
+
+	branch = input->readStr();
+
+	while (branch.empty() == false) {
+		trunk = input->readStr();
+		snapshot = input->readStr();
+		unixtime = input->readInt<uint64_t>();
+
+		auto pair = snapshotBranches.emplace
+			(make_pair(snapshot, vector<string>()));
+
+		pair.first->second.emplace_back(move(branch));
+
+		snapshotDates[snapshot] = unixtime;
+		snapshotTrunks[snapshot] = move(trunk);
+
+		branch = input->readStr();
+	}
+
+	ts.tv_nsec = 0;
+
+	for (const auto &p : snapshotBranches) {
+		ts.tv_sec = snapshotDates[p.first] / 1000000000ul;
+		ts.tv_nsec = snapshotDates[p.first] % 1000000000ul;
+		handler(snapshotTrunks[p.first], p.first, ts, p.second);
+	}
+}
+
+void List_1::_listTrunk(InputStream *input, Handler handler)
+{
+	map<string, vector<string>> snapshotBranches;
+	string branch, snapshot, trunk;
+	struct timespec ts;
+	uint64_t unixtime;
+
+	branch = input->readStr();
+
+	while (branch.empty() == false) {
+		snapshot = input->readStr();
+
+		auto pair = snapshotBranches.emplace
+			(make_pair(snapshot, vector<string>()));
+
+		pair.first->second.emplace_back(move(branch));
+
+		branch = input->readStr();
+	}
+
+	ts.tv_nsec = 0;
+
+	trunk = input->readStr();
+
+	while (trunk.empty() == false) {
+		snapshot = input->readStr();
+
+		while (snapshot.empty() == false) {
+			unixtime = input->readInt<uint64_t>();
+
+			ts.tv_sec = unixtime / 1000000000ul;
+			ts.tv_nsec = unixtime % 1000000000ul;
+
+			handler(trunk, snapshot, ts,
+				snapshotBranches[snapshot]);
+
+			snapshot = input->readStr();
+		}
+
+		trunk = input->readStr();
+	}
+}
+
+void List_1::list(InputStream *input, Handler handler)
+{
+	opcode_t op = input->readInt<opcode_t>();
+
+	if (op == OP_LIST_1_TRUNK)
+		_listTrunk(input, handler);
+	else if (op == OP_LIST_1_BRANCH)
+		_listBranch(input, handler);
+	else
+		throw 0;
+}
