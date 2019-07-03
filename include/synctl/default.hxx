2a9d11049fbdaba585498b6c32362ea9244b136e
Gauthier Voron <gauthier.voron@mnesic.fr>
2019-07-04 00:08:08 +1000

    feat(default): change the default profile search path
    Change the default search path for profile file. It now looks in the
    user dot-files, then in the user config files, then in the system config
    files.

--- include/synctl/default.hxx
+++ include/synctl/default.hxx
@@ -2,24 +2,24 @@
 #define _INCLUDE_SYNCTL_DEFAULT_HXX_
 
 
-#define SYNCTL_CONSTANT_NAME            "synctl"
+#define SYNCTL_CONSTANT_NAME           "synctl"
 
-#define SYNCTL_CONSTANT_MAJOR_VERSION   0
+#define SYNCTL_CONSTANT_MAJOR_VERSION  0
 
-#define SYNCTL_CONSTANT_MINOR_VERSION   0
+#define SYNCTL_CONSTANT_MINOR_VERSION  0
 
-#define SYNCTL_CONSTANT_VARIANT         0
+#define SYNCTL_CONSTANT_VARIANT        0
 
-#define SYNCTL_CONSTANT_AUTHOR          "Gauthier Voron"
+#define SYNCTL_CONSTANT_AUTHOR         "Gauthier Voron"
 
-#define SYNCTL_CONSTANT_MAILTO          "gauthier.voron@mnesic.fr"
+#define SYNCTL_CONSTANT_MAILTO         "gauthier.voron@mnesic.fr"
 
 
-#define SYNCTL_DEFAULT_CONFIG           "/etc/synctl"
+#define SYNCTL_DEFAULT_CONFIG          "~/.synctl:~/.config/synctl:/etc/synctl"
 
-#define SYNCTL_DEFAULT_TRUNK            "master"
+#define SYNCTL_DEFAULT_TRUNK           "master"
 
-#define SYNCTL_DEFAULT_LIST_FORMAT      "human"
+#define SYNCTL_DEFAULT_LIST_FORMAT     "human"
 
 
 #endif
