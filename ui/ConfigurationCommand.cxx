1479801c85a793964b6d89159395707b91426621
Gauthier Voron <gauthier.voron@mnesic.fr>
2019-07-05 19:44:04 +1000

    style(ui): remove a useless namespace specifier
    Import the vector name in ConfigurationCommand and remove the explicit
    namespace specifier where it is used.

--- ui/ConfigurationCommand.cxx
+++ ui/ConfigurationCommand.cxx
@@ -1,6 +1,7 @@
 #include "synctl/ui/ConfigurationCommand.hxx"
 
 #include <string>
+#include <vector>
 
 #include "synctl/ui/ConfigurationBase.hxx"
 #include "synctl/ui/OptionBoolean.hxx"
@@ -8,6 +9,7 @@
 
 
 using std::string;
+using std::vector;
 using synctl::ConfigurationCommand;
 using synctl::OptionBoolean;
 using synctl::OptionString;
@@ -43,7 +45,7 @@ const string &ConfigurationCommand::command() const
 	return _inner->command();
 }
 
-void ConfigurationCommand::getOptions(std::vector<Option *> *dest)
+void ConfigurationCommand::getOptions(vector<Option *> *dest)
 {
 	_inner->getOptions(dest);
 }
